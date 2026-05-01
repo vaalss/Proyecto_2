bf61e2bcd65552a49286f69692af11874441e1a1
SERGIO DANIEL LÓPEZ BARRIENTOS <sergiodaniel007@gmail.com>
2026-05-01 17:44:32 -0600

    Creación Proyecto con Spring Boot

---
+++ proyecto/src/test/java/recomendador_libros/proyecto/ProyectoApplicationTests.java
@@ -0,0 +1,13 @@
+package recomendador_libros.proyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
