56a9d4466f3b0daaa87e460724d2be2a2b397653
DarkFox90 <carlospozuelos3@gmail.com>
2026-05-13 12:32:18 -0600

    agregados métodos de findByEmail y findSimilarByAttributes

--- proyecto/src/main/java/recomendador_libros/proyecto/repositories/UsuarioRepository.java
+++ proyecto/src/main/java/recomendador_libros/proyecto/repositories/UsuarioRepository.java
@@ -7,12 +7,16 @@ import org.springframework.data.neo4j.repository.query.Query;
 
 import recomendador_libros.proyecto.nodes.Libro;
 import recomendador_libros.proyecto.nodes.Usuario;
+import java.util.Optional;
 
 public interface UsuarioRepository extends Neo4jRepository<Usuario, Long> {
+
+    Optional<Usuario> findByEmail(String email);
 
     @Query("MATCH (u:Usuario {nombre: $nombre})-[:LE_GUSTA]->(l:Libro) RETURN l")
     List<Libro> encontrarLibrosQueLeGustan(String nombre);
 
     @Query("MATCH (u:Usuario {nombre: $nombre})-[:HA_LEIDO]->(l:Libro) RETURN l")
     List<Libro> encontrarLibrosLeidos(String nombre);
+
 }
